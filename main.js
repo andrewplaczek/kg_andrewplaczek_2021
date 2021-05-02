1806995a5524edc7f0577c577f50c39ad0ea665c
andrewplaczek <44041712+andrewplaczek@users.noreply.github.com>
2021-05-02 16:43:52 -0400

    Initial Commit
    Created basic structure of program (function names, comments on what each function should do).

---
+++ main.js
@@ -0,0 +1,14 @@
+//Converts Integer array into an array of strings representing the phonetic equivalent of the integer
+function convertArray(intArray){
+
+}
+
+//Converts single integer to phonetic equivalent (e.g 123 will become OneTwoThree)
+function convertInt(x){
+
+}
+
+//Prints a given array of strings to stdout
+function printArray(stringArray){
+
+}
